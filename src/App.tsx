55cbbc774b968e114c6216854f10b10f21ddd26c
Dariusz Okoński <darek200180@gmail.com>
2025-02-12 16:50:50 +0100

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+import { useReducer } from 'react';
 import './App.css';
-import TodoForm from './react-query/TodoForm';
-import TodoList from './react-query/TodoList';
-import Counter from './state-management/Counter';
-import LoginStatus from './state-management/LoginStatus';
 import TaskList from './state-management/TaskList';
+import tasksReducer from './state-management/reducers/tasksReducer';
+import NavBar from './routing/NavBar';
+import HomePage from './routing/HomePage';
+import TasksContext from './state-management/context/tasksContext';
 
 function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
   return (
-    <>
-      <LoginStatus />
-      {/* <TaskList /> */}
-      {/* <Counter /> */}
-      {/* <TodoForm />
-      <TodoList /> */}
-    </>
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
   );
 }
 
